4a7314d67ae7de19fe87bc436cd7602deb2df9af
jennywoites <jenny_200691@hotmail.com>
2017-09-04 16:34:30 +0100

    #1: Se agrega la restriccion de creditos minimos en materias electivas. Se agrega un ParametrosDAO en el que se permite cambiar el numero de ejemplo de materias que se va a cargar. La ecucacion de creditos en materias obligatorias no se agrega ya que existe una restriccion de que todas las materias obligtaorias deben cursarse por lo que resulta redundante

--- Codigo con Pulp/HorariosDAO.py
+++ Codigo con Pulp/HorariosDAO.py
@@ -27,13 +27,37 @@ HORARIOS_3 = {
         "C": [Curso("C", "Curso1C", [Horario(LUNES, 9, 11)]), Curso("C", "Curso2C", [Horario(LUNES, 9, 10)])],
     }
 
-def get_horarios_no_permitidos():
+HORARIOS_4 = {
+        "A": [Curso("A", "Curso1A", [Horario(LUNES, 7, 10),Horario(MARTES,8,12)]), Curso("A", "Curso2A", [Horario(JUEVES, 8, 15),Horario(SABADO,9,13)]), Curso("A", "Curso3A", [Horario(MARTES, 17, 21)])],
+        "B": [Curso("B", "Curso1B", [Horario(LUNES, 7, 10),Horario(JUEVES,7,10)])],
+        "C": [Curso("C", "Curso1C", [Horario(LUNES, 12, 15),Horario(MIERCOLES,8,12)])],
+        "D": [Curso("D", "Curso1D", [Horario(LUNES, 7, 10),Horario(VIERNES,8,12)]), Curso("D", "Curso2D", [Horario(LUNES, 12, 15),Horario(MIERCOLES,8,9)]), Curso("D", "Curso3D", [Horario(JUEVES, 12, 15),Horario(VIERNES,12,15)]), Curso("D", "Curso4D", [Horario(MARTES, 9, 11),Horario(JUEVES,10.5,12.5)])],
+        "E": [Curso("E", "Curso1E", [Horario(LUNES, 7, 10),Horario(VIERNES,8,11)]), Curso("E", "Curso2E", [Horario(MARTES, 8, 15)])],
+        "F": [Curso("F", "Curso1F", [Horario(VIERNES, 8, 13)]), Curso("F", "Curso2F", [Horario(VIERNES, 15, 20)])],
+        "G": [Curso("G", "Curso1G", [Horario(VIERNES, 8, 13)])],
+        "H": [Curso("H", "Curso1H", [Horario(SABADO, 15, 21)])],
+        "I": [Curso("I", "Curso1I", [Horario(SABADO, 21, 21.5)])],
+        "J": [Curso("J", "Curso1J", [Horario(SABADO, 22, 23)])],
+        "K": [Curso("K", "Curso1K", [Horario(MIERCOLES, 9, 12)])],
+        "L": [Curso("L", "Curso1L", [Horario(MIERCOLES, 9, 12)])],
+        "M": [Curso("M", "Curso1M", [Horario(MIERCOLES, 9, 12)])],
+        "N": [Curso("N", "Curso1N", [Horario(MIERCOLES, 9, 12)])],
+    }
+
+HORARIOS = {
+    1: HORARIOS_1,
+    2: HORARIOS_2,
+    3: HORARIOS_3,
+    4: HORARIOS_4
+}
+
+def get_horarios_no_permitidos(num=0):
     """
     Devuelve una lista de horarios en los que el alumno no puede cursar por motivos personales.
     """
     return []
 
 
-def get_horarios():
-    return HORARIOS_1
+def get_horarios(num):
+    return HORARIOS[num]
 
