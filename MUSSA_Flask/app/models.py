6dd8a5c53d14a11d7a05ad4c2f7c9488e747c9f3
jennywoites <jenny_200691@hotmail.com>
2017-09-06 18:26:39 +0100

    #3: Se permite el login y logout desde urls correspondientes

--- MUSSA_Flask/app/models.py
+++ MUSSA_Flask/app/models.py
@@ -2,9 +2,13 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    username = db.Column(db.String(64), index=True, unique=True)
+    password = db.Column(db.String(64), index=True, unique=True)
+    #posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
     @property
     def is_authenticated(self):
@@ -35,14 +39,14 @@ class User(db.Model):
 
 
     def __repr__(self):
-        return '<User %r>' % (self.nickname)
+        return '<User %r>' % (self.username)
 
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#class Post(db.Model):
+#    id = db.Column(db.Integer, primary_key = True)
+#    body = db.Column(db.String(140))
+#    timestamp = db.Column(db.DateTime)
+#    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __repr__(self):
-        return '<Post %r>' % (self.body)
+#    def __repr__(self):
+#        return '<Post %r>' % (self.body)
