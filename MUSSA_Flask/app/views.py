aec38584502d86369b3401f0c898e02eef9d45d8
jennywoites <jenny_200691@hotmail.com>
2017-09-07 16:16:13 +0100

    #3: Se guardan las contraseñas utilizando bcrypt con un salt rounds de 14

--- MUSSA_Flask/app/views.py
+++ MUSSA_Flask/app/views.py
@@ -7,6 +7,7 @@ from app import app, db, lm
 from flask import render_template, flash, redirect, request, session, abort, url_for, g
 from .models import User
 
+import bcrypt
 
 @app.route('/')
 @app.route('/home')
@@ -27,6 +28,7 @@ def login():
     return "Ya estas logueado"
 
 
+SALT_ROUNDS = 14
 @app.route('/login', methods=['POST'])
 def do_admin_login():
     POST_USERNAME = str(request.form['username'])
@@ -34,18 +36,19 @@ def do_admin_login():
  
     if not POST_PASSWORD or not POST_USERNAME:
         flash("Debe ingresar un nombre de usuario y contraseña")
-        return redirect(url_for('login'))
+        return login()
 
     error_msj = "Contraseña incorrecta"
     user = User.query.filter_by(username=POST_USERNAME).first()
 
     if not user: #Solo para agregarlo ahora...
         error_msj = "El usuario no existe"
-        nuevo = User(username=POST_USERNAME, password= POST_PASSWORD)
+        hashed = bcrypt.hashpw(POST_PASSWORD.encode(), bcrypt.gensalt(SALT_ROUNDS))
+        nuevo = User(username=POST_USERNAME, password=hashed)
         db.session.add(nuevo)
         db.session.commit()
 
-    if user and user.username==POST_USERNAME and user.password==POST_PASSWORD:
+    if user and (user.username==POST_USERNAME) and (bcrypt.hashpw(POST_PASSWORD.encode(), user.password) == user.password):
         session['logged_in'] = True
         return redirect('home')
 
