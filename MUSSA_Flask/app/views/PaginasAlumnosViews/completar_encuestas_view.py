e61869dd655b620e3e0319e42e8907c88bc23480
jennywoites <jenny_200691@hotmail.com>
2017-12-08 16:55:40 +0000

    #66: Se agregan la visualización de las encuestas de estrellas, preguntas si/no (las subpreguntas siempre son de tipo texto), y horarios

--- MUSSA_Flask/app/views/PaginasAlumnosViews/completar_encuestas_view.py
+++ MUSSA_Flask/app/views/PaginasAlumnosViews/completar_encuestas_view.py
@@ -3,6 +3,7 @@ from flask_user import login_required
 from app.views.base_view import main_blueprint
 from app.views.Utils.invocaciones_de_servicios import *
 from app.DAO.EncuestasDAO import *
+from app.utils import frange, get_numero_dos_digitos, DIAS
 
 #@main_blueprint.route('/encuestas/completar_encuesta/id_materia_alumno', methods=['GET'])
 @main_blueprint.route('/encuestas/completar_encuesta/general', methods=['GET'])
@@ -38,6 +39,17 @@ def completar_encuesta_docentes_page():
 def completar_encuesta(cookie, num_categoria):
     preguntas = invocar_servicio_obtener_preguntas_encuesta(cookie, [num_categoria])
 
+    HORA_MIN = 7
+    HORA_MAX = 23
+    horarios = []
+    for i in frange(HORA_MIN, HORA_MAX+0.5, 0.5):
+        hora = int(i)
+        minutos = "00" if hora == i else "30"
+        horarios.append("{}:{}".format(get_numero_dos_digitos(hora), minutos))
+
     return render_template('pages/completar_encuesta_page.html',
                            paso_activo = num_categoria,
-                           preguntas = preguntas)
+                           preguntas = preguntas,
+                           dias = DIAS,
+                           hora_desde = horarios[:-1],
+                           hora_hasta = horarios[1:])
