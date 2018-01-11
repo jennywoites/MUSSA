bfeff6368b4971ba61397756eec83b6fc55a78a0
jennywoites <jenny_200691@hotmail.com>
2018-01-11 16:42:31 +0000

    Se actualiza la página de links útiles

---
+++ MUSSA_Flask/app/views/PaginasPublicasViews/links_utiles_view.py
@@ -0,0 +1,48 @@
+from app.views.base_view import main_blueprint
+from flask import render_template
+
+
+@main_blueprint.route('/links_utiles')
+def links_utiles_page():
+    links = [
+        {
+            "URL": 'http://fi.uba.ar/',
+            "MSJ_URL": 'Facultad de Ingeniería',
+            "DESCRIPCION": 'Página oficial de la Facultad de Ingeniería de la Universidad de Buenos Aires'
+        },
+        {
+            "URL": 'http://campus.fi.uba.ar/',
+            "MSJ_URL": 'Campus',
+            "DESCRIPCION": 'Campus virtual FIUBA. Algunas materias utilizan el campus como vía de comunicación o para compartir archivos, subir trabajos prácticos, etc.'
+        },
+        {
+            "URL": 'https://www.facebook.com/groups/fiubaconsultas2/',
+            "MSJ_URL": 'FIUBA Consultas',
+            "DESCRIPCION": 'Grupo de Facebook de alumnos de la FIUBA. En él se hacen consultas muy diversas (incluidas las de ingresantes), algunos chistes, información importante, etc.'
+        },
+        {
+            "URL": 'http://wiki.foros-fiuba.com.ar/',
+            "MSJ_URL": 'Wiki Foros-FIUBA',
+            "DESCRIPCION": 'Ejercicios, trabajos prácticos y exámenes de las distintas materias subidos por alumnos.'
+        },
+        {
+            "URL": 'http://www.foros-fiuba.com.ar/',
+            "MSJ_URL": 'Foros FIUBA',
+            "DESCRIPCION": 'Foro de alumnos de FIUBA. Mucha información ya ha quedado desactualizada pero es posible encontrar información sobre algunas materias y preguntas de diversa índole.'
+        },
+        {
+            "URL": 'http://www.fi.uba.ar/es/node/764',
+            "MSJ_URL": 'Solicitud de Título',
+            "DESCRIPCION": 'Requisitos y trámites necesarios para tramitar el título FIUBA.',
+            "COMENTARIO_OCULTO": 'Si llegaste a este punto congrats! =D'
+        },
+        {
+            "URL": 'http://sietgraduados.rec.uba.ar/',
+            "MSJ_URL": 'Seguimiento del Título',
+            "DESCRIPCION": 'Requisitos y trámites necesarios para tramitar el título FIUBA.',
+            "COMENTARIO_OCULTO": 'Sentate a esperar porque este trámite dura entre un año y medio y dos años :('
+        },
+    ]
+
+    return render_template('pages/links_utiles_page.html',
+                           links=links)
--- MUSSA_Flask/app/views/paginas_publicas_view.py
+++ MUSSA_Flask/app/views/paginas_publicas_view.py
@@ -5,6 +5,7 @@ from flask import render_template
 from app.views.PaginasPublicasViews.buscar_materia_page_view import buscar_materias_page
 from app.views.PaginasPublicasViews.materia_page_view import materia_page
 from app.views.PaginasPublicasViews.comisiones_por_carrera_view import comisiones_por_carrera_page
+from app.views.PaginasPublicasViews.links_utiles_view import links_utiles_page
 
 @main_blueprint.route('/')
 def home_page():
@@ -14,9 +15,3 @@ def home_page():
 @main_blueprint.route('/contacto')
 def contacto_page():
     return render_template('pages/contacto_page.html')
-
-
-@main_blueprint.route('/links_utiles')
-def links_utiles_page():
-    return render_template('pages/links_utiles_page.html')
-
