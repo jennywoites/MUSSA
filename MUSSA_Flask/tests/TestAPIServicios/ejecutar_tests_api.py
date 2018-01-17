ecb766bbd4a45df8c4fda25ff1b024539434f778
jennywoites <jenny_200691@hotmail.com>
2018-01-17 16:03:14 +0000

    #94: Agregados los import a ejecutar tests

--- MUSSA_Flask/tests/TestAPIServicios/ejecutar_tests_api.py
+++ MUSSA_Flask/tests/TestAPIServicios/ejecutar_tests_api.py
@@ -4,12 +4,25 @@ import unittest
 sys.path.append("../..")
 
 # Archivos de test a ejecutar
+from tests.TestAPIServicios.TestCarreraServices.TestBuscarCarreras import TestBuscarCarreras
+from tests.TestAPIServicios.TestCarreraServices.TestObtenerCarrerasDondeSeDictaLaMateria import TestObtenerCarrerasDondeSeDictaLaMateria
+from tests.TestAPIServicios.TestCursoServices.TestBuscarCursos import TestBuscarCursos
+from tests.TestAPIServicios.TestCursoServices.TestObtenerDocentesCurso import TestObtenerDocentesCurso
+from tests.TestAPIServicios.TestDocenteServices.TestObtenerDocentes import TestObtenerDocentes
+from tests.TestAPIServicios.TestMateriaServices.TestBuscarMaterias import TestBuscarMaterias
+from tests.TestAPIServicios.TestMateriaServices.TestObtenerMateria import TestObtenerMateria
+from tests.TestAPIServicios.TestMateriaServices.TestObtenerMateriasCorrelativas import TestObtenerMateriasCorrelativas
+from tests.TestAPIServicios.TestTematicaServices.TestObtenerTematicasMaterias import TestObtenerTematicasMaterias
 
-# - servicios generales -
-
-# - servicios de miembros -
-
-# - servicios de administradores -
+from tests.TestAPIServicios.TestAgregarMateriaAlumno import TestAgregarMateriaAlumno
+from tests.TestAPIServicios.TestGuardarRespuestasEncuestaAlumno import TestGuardarRespuestasEncuestaAlumno
+from tests.TestAPIServicios.TestModificarCurso import TestModificarCurso
+from tests.TestAPIServicios.TestModificarPadronAlumno import TestModificarPadronAlumno
+from tests.TestAPIServicios.TestObtenerEncuestasAlumno import TestObtenerEncuestasAlumno
+from tests.TestAPIServicios.TestObtenerMateriasAlumno import TestObtenerMateriasAlumno
+from tests.TestAPIServicios.TestObtenerPadronAlumno import TestObtenerPadronAlumno
+from tests.TestAPIServicios.TestObtenerPreguntasEncuesta import TestObtenerPreguntasEncuesta
+from tests.TestAPIServicios.TestObtenerRespuestasEncuestaAlumnoParaPreguntasEspecificas import TestObtenerRespuestasEncuestaAlumnoParaPreguntasEspecificas
 
 # Ejecutar todos los tests
 unittest.main()
