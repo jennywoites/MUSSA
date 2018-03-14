1b6c11975a6cac7d6e02a0abe728a08582921d49
jennywoites <jenny_200691@hotmail.com>
2018-03-14 17:06:18 +0000

    #170: Fix de test Test_correlatividades_se_hacen_en_orden_varios_niveles_correlatividades

--- MUSSA_Flask/tests/TestResolucionPlanDeEstudios/MateriasDAOMock.py
+++ MUSSA_Flask/tests/TestResolucionPlanDeEstudios/MateriasDAOMock.py
@@ -8,16 +8,46 @@ from app.API_Rest.GeneradorPlanCarreras.Constantes import *
 ##              Horarios                   ##
 #############################################
 
+LUNES_7_7y5 = Horario(
+    dia=LUNES,
+    hora_inicio=7,
+    hora_fin=7.5
+)
+
+LUNES_7y5_8 = Horario(
+    dia=LUNES,
+    hora_inicio=7.5,
+    hora_fin=8
+)
+
 LUNES_7_8 = Horario(
     dia=LUNES,
     hora_inicio=7,
     hora_fin=8
 )
 
+LUNES_8_8y5 = Horario(
+    dia=LUNES,
+    hora_inicio=8,
+    hora_fin=8.5
+)
+
 LUNES_8_9 = Horario(
     dia=LUNES,
     hora_inicio=8,
     hora_fin=9
+)
+
+LUNES_8y5_9 = Horario(
+    dia=LUNES,
+    hora_inicio=8.5,
+    hora_fin=9
+)
+
+LUNES_9_9y5 = Horario(
+    dia=LUNES,
+    hora_inicio=9,
+    hora_fin=9.5
 )
 
 LUNES_9_10 = Horario(
@@ -74,6 +104,16 @@ CURSO_2_MATERIA_A = Curso(
     puntaje=0
 )
 
+CURSO_3_MATERIA_A = Curso(
+    id_curso='6',
+    cod_materia='A',
+    nombre_curso='3A',
+    horarios=[LUNES_7_7y5],
+    se_dicta_primer_cuatrimestre=True,
+    se_dicta_segundo_cuatrimestre=True,
+    puntaje=0
+)
+
 ##################################################
 MATERIA_B_OBLIGATORIA = Materia(
     id_materia='2',
@@ -106,6 +146,16 @@ CURSO_2_MATERIA_B = Curso(
     se_dicta_segundo_cuatrimestre=False,
     puntaje=0
 )
+
+CURSO_3_MATERIA_B = Curso(
+    id_curso='7',
+    cod_materia='B',
+    nombre_curso='3B',
+    horarios=[LUNES_7y5_8],
+    se_dicta_primer_cuatrimestre=True,
+    se_dicta_segundo_cuatrimestre=True,
+    puntaje=0
+)
 ##############################################
 MATERIA_C_OBLIGATORIA = Materia(
     id_materia='3',
@@ -124,6 +174,60 @@ CURSO_1_MATERIA_C = Curso(
     cod_materia='C',
     nombre_curso='1C',
     horarios=[LUNES_9_10],
+    se_dicta_primer_cuatrimestre=True,
+    se_dicta_segundo_cuatrimestre=True,
+    puntaje=0
+)
+
+CURSO_2_MATERIA_C = Curso(
+    id_curso='8',
+    cod_materia='C',
+    nombre_curso='2C',
+    horarios=[LUNES_8_8y5],
+    se_dicta_primer_cuatrimestre=True,
+    se_dicta_segundo_cuatrimestre=True,
+    puntaje=0
+)
+#############################################
+MATERIA_D_OBLIGATORIA = Materia(
+    id_materia='4',
+    codigo='D',
+    nombre='Materia D',
+    creditos=6,
+    tipo=OBLIGATORIA,
+    cred_min=0,
+    correlativas=[MATERIA_B_OBLIGATORIA.id_materia, MATERIA_C_OBLIGATORIA.id_materia],
+    tematicas_principales=[],
+    medias_horas_extras_cursada=12
+)
+
+CURSO_1_MATERIA_D = Curso(
+    id_curso='9',
+    cod_materia='D',
+    nombre_curso='1D',
+    horarios=[LUNES_8y5_9],
+    se_dicta_primer_cuatrimestre=True,
+    se_dicta_segundo_cuatrimestre=True,
+    puntaje=0
+)
+##############################################
+MATERIA_E_OBLIGATORIA = Materia(
+    id_materia='5',
+    codigo='E',
+    nombre='Materia E',
+    creditos=6,
+    tipo=OBLIGATORIA,
+    cred_min=0,
+    correlativas=[],
+    tematicas_principales=[],
+    medias_horas_extras_cursada=12
+)
+
+CURSO_1_MATERIA_E = Curso(
+    id_curso='10',
+    cod_materia='E',
+    nombre_curso='1E',
+    horarios=[LUNES_9_9y5],
     se_dicta_primer_cuatrimestre=True,
     se_dicta_segundo_cuatrimestre=True,
     puntaje=0
