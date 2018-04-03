4dfab6eaf38604d78ecb2015ef73096f61ff43bf
jennywoites <jenny_200691@hotmail.com>
2018-04-03 12:32:59 +0100

    #188: Fallaba al elimina encuesta alumno porque no se estaba eliminado la clase Respuesta encuesta alumno asociada a esa encuesta

--- MUSSA_Flask/app/API_Rest/Services/AlumnoServices/MateriaAlumnoService.py
+++ MUSSA_Flask/app/API_Rest/Services/AlumnoServices/MateriaAlumnoService.py
@@ -268,6 +268,9 @@ class MateriaAlumnoService(BaseService):
         servicio_respuesta_encuesta.eliminar_respuestas(respuestas_encuestas)
         db.session.commit()
 
+        RespuestaEncuestaAlumno.query.filter_by(encuesta_alumno_id=encuesta.id).delete()
+        db.session.commit()
+
         EstadoPasosEncuestaAlumno.query.filter_by(encuesta_alumno_id=encuesta.id).delete()
         db.session.commit()
 
