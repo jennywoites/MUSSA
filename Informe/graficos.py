8d2ecbb39175e3398b4021bace67bf457ee3d1e9
jennywoites <jenny_200691@hotmail.com>
2018-04-04 11:15:16 +0100

    #187: Completo usuario administrador y agradecimientos

---
+++ Informe/graficos.py
@@ -0,0 +1,9 @@
+
+# pip install matplotlib
+# sudo apt install python3-tk
+datos1 = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]]
+datos2 = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]]
+import matplotlib.pyplot as plt
+plt.plot(*datos1)
+plt.plot(*datos2)
+plt.show()
